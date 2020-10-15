1d79e7f701fe3a2a6e002a409470db18abfc063b
Kirill Kondratyuk <kirkondratiuk@gmail.com>
2020-10-15 22:28:38 +0300

    start

---
+++ Source/ChivGame/ChivGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ChivGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CHIVGAME_API AChivGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
